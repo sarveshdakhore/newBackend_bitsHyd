33d8b18dcfbafdf88ced1e8dd05bba8957fd6bcd
JothishKamal <jothishkamal@gmail.com>
2025-03-23 10:24:08 +0530

    feat: user info routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,13 @@ import sign from "./sign.js";
 import home from "./home.js";
 import ml from "./ml.js";
 import forms from "./forms.js";
+import users from "./users.js";
 
 const router = express.Router();
 router.use("/sign", sign);
 router.use("/", home);
 router.use("/ml", ml);
 router.use("/forms", forms);
+router.use("/users", users);
 
 export default router;
